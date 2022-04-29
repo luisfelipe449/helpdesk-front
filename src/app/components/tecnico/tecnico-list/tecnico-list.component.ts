d6808fd42260e576c5ea34f3297a731667b34505
Luis <1luisfelipe2@gmail.com>
2022-04-28 22:37:37 -0400

    html e css para tecnico-list

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+import { Tecnico } from "src/app/models/tecnico";
 
 @Component({
-  selector: 'app-tecnico-list',
-  templateUrl: './tecnico-list.component.html',
-  styleUrls: ['./tecnico-list.component.css']
+  selector: "app-tecnico-list",
+  templateUrl: "./tecnico-list.component.html",
+  styleUrls: ["./tecnico-list.component.css"],
 })
 export class TecnicoListComponent implements OnInit {
+  displayedColumns: string[] = [
+    "id",
+    "nome",
+    "cpf",
+    "email",
+    "acoes",
+  ];
 
-  constructor() { }
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: "Tecnico 1",
+      email: "tecnico@mail.com",
+      cpf: "000.000.000-00",
+      senha: "123",
+      perfis: ["ADMIN"],
+      dataCriacao: new Date(),
+    },
+  ];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
-
 }
---
+++ src/app/models/tecnico.ts
@@ -0,0 +1,9 @@
+export interface Tecnico {
+    id?: any;
+    nome: string;
+    email: string;
+    cpf: string;
+    senha: string;
+    perfis: string[];
+    dataCriacao: any;
+}
