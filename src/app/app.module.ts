d9a622fce890164e41ca93b30212259aa589731f
Luis <1luisfelipe2@gmail.com>
2022-04-28 23:29:56 -0400

    interceptor para requisicoes http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { HeaderComponent } from "./components/header/header.component";
 import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
 import { LoginComponent } from "./components/login/login.component";
 import { ToastrModule } from "ngx-toastr";
+import { AuthInterceptorProvider } from "./interceptors/auth.interceptor";
 
 @NgModule({
   declarations: [
@@ -68,7 +69,7 @@ import { ToastrModule } from "ngx-toastr";
       progressBar: true,
     }),
   ],
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
