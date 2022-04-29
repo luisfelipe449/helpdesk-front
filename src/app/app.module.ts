cb0019223fa95428985de87ccb7790d78123042f
Luis Felipe <1luisfelipe2@gmail.com>
2022-04-29 13:59:34 -0400

    model chamado criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { ClienteCreateComponent } from "./components/cliente/cliente-create/clie
 import { ClienteDeleteComponent } from "./components/cliente/cliente-delete/cliente-delete.component";
 import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 @NgModule({
   declarations: [
@@ -52,6 +53,7 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
     ClienteDeleteComponent,
     ClienteListComponent,
     ClienteUpdateComponent,
+    ChamadoListComponent,
   ],
   imports: [
     BrowserModule,
